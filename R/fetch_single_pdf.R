6cadee896eea5fd602b0bd124a3344fda1125fa0
Renan Xavier Cortes <renan.cortes@agibank.com.br>
2020-04-18 23:12:09 -0300

    first commit

---
+++ R/fetch_single_pdf.R
@@ -0,0 +1,26 @@
+# Function that fetchs the pdf file and saves it in the current directory
+
+fecth_single_pdf <- function(book_title, springer_table){
+
+  file_sep <- .Platform$file.sep
+
+  aux <- springer_table %>%
+    filter(`Book Title` == book_title)
+
+  author <- aux$Author
+  en_book_type <- aux$`English Package Name`
+
+  download_url <- aux$OpenURL %>%
+    GET() %>%
+    extract2('url') %>%
+    str_replace('book', paste0('content', file_sep, 'pdf')) %>%
+    str_replace('%2F', file_sep) %>%
+    paste0('.pdf')
+
+  pdf_file = GET(download_url)
+
+  write.filename = file(paste0(book_title, " - ", author, ".pdf"), "wb")
+  writeBin(pdf_file$content, write.filename)
+  close(write.filename)
+
+}
---
+++ R/generate_pdf_list.R
@@ -0,0 +1,37 @@
+# Generate all pdf's in the directories organized by book type
+
+generate_pdf_files <- function(springer_books_titles = NA, springer_table = NA) {
+
+if (is.na(springer_table)) {
+  books_list_url <- 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v4/'
+  GET(books_list_url, write_disk(tf <- tempfile(fileext = ".xlsx")))
+  springer_table <- read_excel(tf)
+}
+
+if (is.na(springer_books_titles)) { springer_books_titles <- springer_table$`Book Title`}
+
+n <- length(springer_books_titles)
+
+i <- 1
+
+for (title in springer_books_titles) {
+
+  print(paste0('Processing... ', title, '(', i, ' out of ', n, ')'))
+
+  en_book_type <- springer_table %>%
+    filter(`Book Title` == title) %>%
+    pull(`English Package Name`)
+
+  current_folder = file.path('springer_downloads', en_book_type)
+  if (!dir.exists(current_folder)) { dir.create(current_folder, recursive = T) }
+  setwd(current_folder)
+  tic(paste0('Time processed of', title))
+  fecth_single_pdf(title, springer_table)
+  toc()
+  setwd(file.path('.', '..', '..'))
+
+  i <- i + 1
+
+}
+
+}
