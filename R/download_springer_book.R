222579d05acef2b3a178d859a7add590e34a8a85
adripo <adrian.polimi@gmail.com>
2020-04-28 18:31:23 +0200

    Added option to download pdf and epub files

--- R/download_springer_book.R
+++ R/download_springer_book.R
@@ -1,6 +1,6 @@
 # Function that fetchs the pdf file and saves it in the current directory
 
-download_springer_book <- function(book_spec_title, springer_table){
+download_springer_book <- function(book_spec_title, springer_table, filetype){
 
   file_sep <- .Platform$file.sep
 
@@ -11,21 +11,30 @@ download_springer_book <- function(book_spec_title, springer_table){
 
   edition <- aux$edition
   en_book_type <- aux$english_package_name
+  
+  if(filetype == 'pdf'){
+    dlpath <- 'content'
+  }
+  else if(filetype == 'epub'){
+    dlpath <- 'download'
+  }
 
   download_url <- aux$open_url %>%
     GET() %>%
     extract2('url') %>%
-    str_replace('book', paste0('content', file_sep, 'pdf')) %>%
+    str_replace('book', paste0(dlpath, file_sep, filetype)) %>%
     str_replace('%2F', file_sep) %>%
-    paste0('.pdf')
+    paste0('.', filetype)
 
   pdf_file = GET(download_url)
 
-  clean_book_title <- str_replace(book_spec_title, '/', '-') # Avoiding '/' special character in filename
-  clean_book_title <- str_replace(clean_book_title, ':', '-') # Avoiding ':' special character in filename
+  if(!http_error(pdf_file)){
+    clean_book_title <- str_replace(book_spec_title, '/', '-') # Avoiding '/' special character in filename
+    clean_book_title <- str_replace(clean_book_title, ':', '-') # Avoiding ':' special character in filename
 
-  write.filename = file(paste0(clean_book_title, " - ", edition, ".pdf"), "wb")
-  writeBin(pdf_file$content, write.filename)
-  close(write.filename)
+    write.filename = file(paste0(clean_book_title, " - ", edition, ".", filetype), "wb")
+    writeBin(pdf_file$content, write.filename)
+    close(write.filename)
+  }
 
 }
